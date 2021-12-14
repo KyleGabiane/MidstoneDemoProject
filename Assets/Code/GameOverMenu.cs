e496779f5813a03873c7a687ba9065ed24cbf93c
KyleGabiane <72885112+KyleGabiane@users.noreply.github.com>
2021-12-14 03:31:59 -0500

    Replacing old scenes with Updated Scenes
    Have to replace the old scenes that I did previously with updated scenes that I did currently.

---
+++ Assets/Code/GameOverMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
